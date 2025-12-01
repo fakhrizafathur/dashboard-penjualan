50c1464d5259681d70a6e7189940d3d677fbf884
fakhrizafathur <fakhrizafathur05@gmail.com>
2025-12-02 00:07:28 +0700

    cached

--- routes/web.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use App\Http\Controllers\SalesController;
-use Illuminate\Support\Facades\Route;
-
-Route::get('/dashboard', [SalesController::class, 'index'])->name('dashboard.sales');
-Route::redirect('/', '/dashboard');
